f3ba86afa4a975eb9f67e823e37494100ad37842
Henna Maria <hennamaria2001@gmail.com>
2023-11-30 16:31:36 +0530

    props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Cards from "./component/Cards"
+
+function App() {
+
+  return (
+    <>
+     <Cards title="Utility Jacket"/>
+     <Cards title="Polo Jacket"/>
+    </>
+  )
+}
+
+export default App
